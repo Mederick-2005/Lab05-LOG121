9f4824add95d05273f318358e1470deb85e43916
VicDubuc <victor.dubuc.1@ens.etsmtl.ca>
2025-11-28 00:11:44 -0500

    Victor Dubuc reglage bug charger image

--- src/controleur/commande/CommandeChargerImage.java
+++ src/controleur/commande/CommandeChargerImage.java
@@ -15,8 +15,14 @@ public class CommandeChargerImage extends CommandeModele{
         if(resultat == JFileChooser.APPROVE_OPTION) {
             File file = chooser.getSelectedFile();
             controleurInterface.getModelePrincipale().chargerImage(file);
+
             controleurInterface.getModelePerspective1().chargerImage(file);
+            controleurInterface.getModelePerspective1().setZoom(1);
+            controleurInterface.getModelePerspective1().mouvement(0,0);
+
             controleurInterface.getModelePerspective2().chargerImage(file);
+            controleurInterface.getModelePerspective2().setZoom(1);
+            controleurInterface.getModelePerspective2().mouvement(0,0);
         }
     }
     public void executer(){}
