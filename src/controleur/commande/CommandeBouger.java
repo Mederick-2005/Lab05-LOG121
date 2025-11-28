b0e2086d55f52409f94ce3e372ef18a3244b1a6a
VicDubuc <victor.dubuc.1@ens.etsmtl.ca>
2025-11-27 23:37:32 -0500

    Victor Dubuc classes commande zoom et commande bouger finies, il faut régler le update des vues en décidant un rapport molette/zoom et mettre des limites au zoom et deplacement

--- src/controleur/commande/CommandeBouger.java
+++ src/controleur/commande/CommandeBouger.java
@@ -2,8 +2,30 @@ package controleur.commande;
 import Modele.ImagePerspective;
 
 public class CommandeBouger extends CommandePerspective{
+    private double valeurX, valeurY;
+    private ImagePerspective imageSelec;
 
-    public CommandeBouger(double ValeurX, double ValeurY, ImagePerspective imageSelec) {}
-    public void executer(){}
-    public void annuler(){}
+    public CommandeBouger(double ValeurX, double ValeurY, ImagePerspective imageSelec) {
+        this.valeurX = ValeurX;
+        this.valeurY = ValeurY;
+        this.imageSelec = imageSelec;
+    }
+
+    /**
+     * Déplacement de l'image
+     */
+    public void executer(){
+        double ancienneValeurX = imageSelec.getDeplacementX();
+        double ancienneValeurY = imageSelec.getDeplacementY();
+        imageSelec.mouvement(ancienneValeurX + valeurX, ancienneValeurY + valeurY);
+    }
+
+    /**
+     * Annulation du déplacement en faisant le déplacement inverse
+     */
+    public void annuler(){
+        double ancienneValeurX = imageSelec.getDeplacementX();
+        double ancienneValeurY = imageSelec.getDeplacementY();
+        imageSelec.mouvement(ancienneValeurX - valeurX, ancienneValeurY - valeurY);
+    }
 }
--- src/controleur/commande/CommandeZoom.java
+++ src/controleur/commande/CommandeZoom.java
@@ -3,8 +3,27 @@ package controleur.commande;
 import Modele.ImagePerspective;
 
 public class CommandeZoom extends CommandePerspective{
+    private double niveauZoom;
+    private ImagePerspective imageSelec;
 
-    public CommandeZoom(double niveauZoom, ImagePerspective imageSelec) {}
-    public void executer(){}
-    public void annuler(){}
+    public CommandeZoom(double niveauZoom, ImagePerspective imageSelec) {
+        this.niveauZoom = niveauZoom;
+        this.imageSelec = imageSelec;
+    }
+
+    /**
+     * exécute la zoom
+     */
+    public void executer(){
+        double ancienZoom = imageSelec.getZoom();
+        imageSelec.setZoom(ancienZoom + niveauZoom);
+    }
+
+    /**
+     * Annule le zoom en faisant un zoom de la valeur inverse du zoom initial
+     */
+    public void annuler(){
+        double ancienZoom = imageSelec.getZoom();
+        imageSelec.setZoom(ancienZoom - niveauZoom);
+    }
 }
